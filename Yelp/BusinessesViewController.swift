f308efe65eb99b31dbb2f5624d9ef56e346745db
Anthony Bravo <bravo@Anthonys-MBP.fios-router.home>
2018-10-21 02:27:09 -0400

    added search bar

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -1,18 +1,14 @@
-//
-//  BusinessesViewController.swift
-//  Yelp
-//
-//  Created by Timothy Lee on 4/23/15.
-//  Copyright (c) 2015 Timothy Lee. All rights reserved.
-//
 
 import UIKit
 
-class BusinessesViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+class BusinessesViewController: UIViewController,UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     
     var businesses: [Business]!
     var searchBar : UISearchBar!
-    var searchController : UISearchController!
+    var businessesNamesOriginal : [String] = []
+    var businessesNames : [String] = [] //holds updated names from search
+    var businessesUpdated: [Business]! = [] //holds updates businesses from search
+
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -23,20 +19,14 @@ class BusinessesViewController: UIViewController,UITableViewDataSource, UITableV
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 120
         
+        //Creating search bar
         searchBar = UISearchBar()
         searchBar.sizeToFit()
+        searchBar.delegate = self
+        
+        //Places search bar on the top
         navigationItem.titleView = searchBar
-        searchDisplayController?.displaysSearchBarInNavigationBar = true
         
-         searchController = UISearchController(searchResultsController: nil)
-        
-        
-         searchController.searchBar.sizeToFit()
-         navigationItem.titleView = searchController.searchBar
-        searchController.hidesNavigationBarDuringPresentation = false
-
- 
-
 
         
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
@@ -47,7 +37,11 @@ class BusinessesViewController: UIViewController,UITableViewDataSource, UITableV
                     for business in businesses {
                         print(business.name!)
                         print(business.address!)
+                        self.businessesNames.append(business.name!)
+                        self.businessesNamesOriginal.append(business.name!)
                     }
+                    self.businessesUpdated = self.businesses
+                    self.tableView.reloadData()
                 }
             
             }
@@ -82,7 +76,7 @@ class BusinessesViewController: UIViewController,UITableViewDataSource, UITableV
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if businesses != nil {
-            return businesses!.count
+            return businessesNames.count
         }
         else {
             return 0;
@@ -91,9 +85,43 @@ class BusinessesViewController: UIViewController,UITableViewDataSource, UITableV
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
-        cell.business = businesses[indexPath.row]
+        
+        cell.business = businessesUpdated[indexPath.row]
         return cell
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //holds list of business names that matches our search
+        businessesNames = searchText.isEmpty ? businessesNamesOriginal:businessesNames.filter{(item: String) -> Bool in
+            return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        if searchText.isEmpty {
+            businessesUpdated = self.businesses
+        }
+        else {
+            var i  = 0
+            //Delete businesses that dont match our search return from the temp business array
+            for business in businessesUpdated {
+                if(!businessesNames.contains(business.name!)) {
+                    businessesUpdated.remove(at: i)
+                    i = i - 1
+                }
+                i = i + 1
+            }
+        }
+        tableView.reloadData()
+    }
+    
+    
+    //Hide keyboard
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        searchBar.endEditing(true)
+    }
+    
     
     
     
